6bcdd477a103df8b6035ed8ec04f9788d43d3c07
Thewaveq <132695815+Thewaveq@users.noreply.github.com>
2025-07-22 17:51:33 +0300

    Delete firebase-config.template.js

--- firebase-config.template.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "__NEXT_PUBLIC_FIREBASE_API_KEY__",
-    authDomain: "__NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN__",
-    projectId: "__NEXT_PUBLIC_FIREBASE_PROJECT_ID__",
-    storageBucket: "__NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET__",
-    messagingSenderId: "__NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID__",
-    appId: "__NEXT_PUBLIC_FIREBASE_APP_ID__",
-    measurementId: "__NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID__"
-};

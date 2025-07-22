9a5f595856050b202616d32054a6b014f082fe8c
Thewaveq <132695815+Thewaveq@users.noreply.github.com>
2025-07-22 10:29:18 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDZ3u9scjk3dydutXO0EUrWlpUnsp4OuqA",
+    authDomain: "appllm.firebaseapp.com",
+    projectId: "appllm",
+    storageBucket: "appllm.appspot.com",
+    messagingSenderId: "926733538345",
+    appId: "1:926733538345:web:dac990973b29ca574ff938",
+    measurementId: "G-YX6FY680C9"
+};
